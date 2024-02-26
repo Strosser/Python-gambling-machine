5d25625401de305a256d9a6ad83d1d32facc3dce
Strosser <jan.strosser44@gmail.com>
2024-02-26 15:36:20 +0100

    Inputs and simple calucation integration

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+MAX_LINES = 3
+MAX_BET = 1000
+MIN_BET = 1
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount has to be grater than 0.")
+        else:
+            print("Please enter number.")
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    return amount
 
+def get_nubmber_of_lines():
+    while True:
+        lines = input("How many lines would you pick bet on (1-" + str(MAX_LINES) + ")?  ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter number.")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter number.")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    return amount
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    balance = deposit()
+    lines = get_nubmber_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough balance to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+main()
